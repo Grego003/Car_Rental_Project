e1390b72a6d67696ba4430d754287a407beb327f
stuvvy <hjaoel@gmail.com>
2023-11-05 02:13:05 +0700

    rapihin login dan boilerplate card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha10")
     implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- app/src/main/java/com/example/car_rental_project/composable/home/HomeScreen.kt
+++ app/src/main/java/com/example/car_rental_project/composable/home/HomeScreen.kt
@@ -1,29 +1,41 @@
 package com.example.car_rental_project.composable.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.car_rental_project.R
+import com.example.car_rental_project.model.CarModel
 import com.example.car_rental_project.model.UserModel
 
 //Todo Rapihin ama buat design nya (dipecah pecah kaya login)
@@ -31,7 +43,10 @@ import com.example.car_rental_project.model.UserModel
 fun HomeScreen(
     userData : UserModel?,
     onSignOut : () -> Unit,
+    carList: List<CarModel>,
+
 ) {
+
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -47,7 +62,6 @@ fun HomeScreen(
             verticalAlignment  = Alignment.CenterVertically,
 
         ) {
-            //todo bikin button jadi kaya spinner kemaren
             Button(onClick = onSignOut) { Text(text = "Sign Out") }
             Spacer(modifier = Modifier.weight(1f))
             Text (
@@ -65,9 +79,67 @@ fun HomeScreen(
                 fallback = painterResource(id = R.drawable.profile_placeholder),
             )
         }
+
+        val (rightColumn, leftColumn) = carList.splitAt(carList.size / 2)
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(leftColumn) { car -> CarCard(car) }
+            }
+
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(rightColumn) { car -> CarCard(car) }
+            }
+        }
     }
 }
 
+// GATAU INI APAAN
+fun <T> List<T>.splitAt(index: Int): Pair<List<T>, List<T>> {
+    return Pair(subList(0, index), subList(index, size))
+}
+
+@Composable
+fun CarCard(car: CarModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.background)
+            .clickable {}
+    ) {
+        Image(
+            painter = painterResource(id = car.imageResource),
+            contentDescription = stringResource(id = R.string.car_list_description),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(125.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "${car.brand} ${car.model}",
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = car.price,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+}
 
 
 @Preview(showBackground = true)
@@ -79,6 +151,13 @@ fun HomeScreenReview() {
         profilePicture = null
     )
 
-    HomeScreen(dummyUserData, onSignOut = {})
-}
+    val carList = listOf(
+        CarModel("1", "Toyota", "Corolla", "$10,000", R.drawable.placeholder_logo),
+        CarModel("2", "Honda", "Civic", "$9,500", R.drawable.placeholder_logo),
+        CarModel("3", "Ford", "Mustang", "$20,000", R.drawable.placeholder_logo),
+        CarModel("4", "Honda", "Brio", "$4,000", R.drawable.placeholder_logo),
+        CarModel("5", "Audi", "I8", "$34,000", R.drawable.placeholder_logo),
+        )
 
+    HomeScreen(dummyUserData, onSignOut = {}, carList)
+}
