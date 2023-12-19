b5d06026f4c990c94893eb7d4b413e488cd1028e
Gregorius Ivan Halim <99463068+Grego003@users.noreply.github.com>
2023-12-19 23:34:01 +0700

    dosent break anymore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,5 +84,4 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-
 }
